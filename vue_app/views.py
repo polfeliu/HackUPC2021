c1c5236ea7e0253870ac26ae6e4817a48486c229
Pol Feliu <plo.fc.123@gmail.com>
2021-05-15 11:51:23 +0200

    example of how to retrieve data from database and display it on a template

--- vue_app/views.py
+++ vue_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from vue_app.models import Idea
+
+
 def feed(request):
-    return HttpResponse("Hello, this is the feed")
+
+    context = {
+        'myvar': 'helooooo',
+        'posts': Idea.objects.all()
+    }
+    return render(request, 'vue_app/test.html', context)
 
 def post(request, post_id):
     return HttpResponse(f"Hello, this is the {post_id}")
